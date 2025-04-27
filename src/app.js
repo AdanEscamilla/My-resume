2df6eed806c95b5f9de6b096e6c0cd487c1bfe73
Adan Escamilla Hurtado <adan.escamilla@uabc.edu.mx>
2025-04-26 17:01:39 -0700

    My-Resume-V2.1
    My-Resume-V2.1

---
+++ src/app.js
@@ -0,0 +1,10 @@
+$(function() {
+    // Close and open mobile menu
+    $('#open-menu').on("click", function() {
+        $('#mobile-menu').show();
+    });
+
+    $('#close-menu').on("click", function() {
+        $('#mobile-menu').hide();
+    });
+});
